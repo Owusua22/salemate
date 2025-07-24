e2e13aa1b1d25cc9dc33c31f19973bac93682484
Owusua22 <nanaowusua1996@gmail.com>
2025-07-24 09:50:16 +0000

    update

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function createWindow() {
 
     const win = new BrowserWindow({
       width: 1000,
-      height: 800,
+      height: 700,
       webPreferences: {
         nodeIntegration: true,
         contextIsolation: false,
